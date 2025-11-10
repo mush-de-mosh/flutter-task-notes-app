3835a460fc6a0d833003a982ff87023f97d407f2
Moses Mushabe <mushdemosh143@gmail.com>
2025-11-10 10:28:22 +0300

    create TaskItem model class with properties

---
+++ lib/models/task_item.dart
@@ -0,0 +1,15 @@
+class TaskItem {
+  final String id;
+  final String title;
+  final String priority;
+  final String description;
+  final bool isCompleted;
+
+  TaskItem({
+    required this.id,
+    required this.title,
+    required this.priority,
+    required this.description,
+    required this.isCompleted,
+  });
+}
